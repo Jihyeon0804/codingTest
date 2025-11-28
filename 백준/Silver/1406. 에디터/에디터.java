516a095b0f0775bf8cdcf9a07a2179076e89936d
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2025-11-29 02:22:16 +0900

    [Silver II] Title: 에디터, Time: 452 ms, Memory: 74504 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,38 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder(br.readLine());
-        int idx = sb.length();
+        String s = br.readLine();
         int n = Integer.parseInt(br.readLine());
 
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            left.push(c);
+        }
 
         for (int i = 0; i < n; i++) {
-            String[] arr = br.readLine().split(" ");
-            if (arr[0].equals("L")) {
-                if (idx != 0) {
-                    idx--;
-                }
-            } else if (arr[0].equals("D")) {
-                if (idx != sb.length()) {
-                    idx++;
-                }
-            } else if (arr[0].equals("B")) {
-                if (idx != 0) {
-                    sb.deleteCharAt(idx - 1);
-                    idx--;
-                }
-            } else if (arr[0].equals("P")){
-                sb.insert(idx, arr[1]);
-                idx++;
-            }
+            String str = br.readLine();
+            if (str.charAt(0) == 'L') {
+                if (!left.isEmpty()) {
+                    right.push(left.pop());
 
+                }
+            } else if (str.charAt(0) == 'D') {
+                if (!right.isEmpty()) {
+                    left.push(right.pop());
+                }
+            } else if (str.charAt(0) == 'B') {
+                if (!left.isEmpty()) {
+                    left.pop();
+                }
+            } else if (str.charAt(0) == 'P') {
+                left.push(str.charAt(str.length() - 1));
+            }
         }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!left.isEmpty()) {
+            right.push(left.pop());
+        }
+
+        while (!right.isEmpty()) {
+            sb.append(right.pop());
+        }
+
         System.out.println(sb);
 
     }
