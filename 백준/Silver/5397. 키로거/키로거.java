c2058c762c2d29627cf7a0ae7b52b73a4403fcb6
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2025-12-01 11:53:39 +0900

    [Silver II] Title: 키로거, Time: 512 ms, Memory: 58412 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,58 +1,83 @@
 import java.io.*;
-import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static final int MX = 1000005;
+    static int[] pre = new int[MX];
+    static int[] nxt = new int[MX];
+    static char[] dat = new char[MX];
+    static int unused = 1;
+    static int cursor = 0;
 
+    public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
-
-        for (int i  = 0 ; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            sb.append(getPassword(s));
+            run(s);
+            
+            int addr = nxt[0];
+            while (addr != -1) {
+                sb.append(dat[addr]);
+                addr = nxt[addr];
+            }
             sb.append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    static String getPassword(String string) {
+    static void run(String s) {
+        unused = 1;
+        cursor = 0;
+        nxt[0] = -1; 
+        pre[0] = -1;
 
-        if (string.matches("^[a-zA-Z0-9]+$")) {
-            return string;
-        } else {
-            Stack<Character> left = new Stack<>();
-            Stack<Character> right = new Stack<>();
-            for (char c : string.toCharArray()) {
-                if (c == '<') {
-                    if (!left.isEmpty()) {
-                        right.push(left.pop());
-                    }
-                } else if (c == '>') {
-                    if (!right.isEmpty()) {
-                        left.push(right.pop());
-                    }
-                } else if (c == '-') {
-                    if (!left.isEmpty()) {
-                        left.pop();
-                    }
-                } else {
-                    left.push(c);
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '<') {
+                if (pre[cursor] != -1) {
+                    cursor = pre[cursor];
                 }
+            } else if (c == '>') {
+                if (nxt[cursor] != -1) {
+                    cursor = nxt[cursor];
+                }
+            } else if (c == '-') {
+                if (cursor != 0) {
+                    erase();
+                }
+            } else {
+                insert(c);
             }
-
-            StringBuilder sb = new StringBuilder();
-
-            while (!left.isEmpty()) {
-                right.push(left.pop());
-            }
-
-            while (!right.isEmpty()) {
-                sb.append(right.pop());
-            }
-            return sb.toString();
         }
+    }
+
+    static void insert(char c) {
+        dat[unused] = c;
+        pre[unused] = cursor;
+        nxt[unused] = nxt[cursor];
+
+        if (nxt[cursor] != -1) {
+            pre[nxt[cursor]] = unused;
+        }
+
+        nxt[cursor] = unused;
+
+        cursor = unused;
+        unused++;
+    }
+
+    static void erase() {
+        if (nxt[cursor] != -1) {
+            pre[nxt[cursor]] = pre[cursor];
+        }
+        
+        nxt[pre[cursor]] = nxt[cursor];
+        
+        cursor = pre[cursor];
     }
 }
