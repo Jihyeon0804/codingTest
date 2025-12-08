babe05789dfddb321210fb6b7168f08b5b5e5b2d
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2025-12-08 11:46:38 +0900

    [Silver II] Title: A → B, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int cnt = 1;
+        while (a < b) {
+            if (b % 10 == 1) {
+                b /= 10;
+            } else if (b % 2 == 0) {
+                b /= 2;
+            } else {
+                break;
+            }
+            cnt++;
+        }
+        System.out.println(a == b ? cnt : -1);
+    }
+}
