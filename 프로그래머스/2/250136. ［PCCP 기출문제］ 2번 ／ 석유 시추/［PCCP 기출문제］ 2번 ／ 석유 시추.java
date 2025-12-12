b0a7379816e2d48e310da4b557eb7f69d1b4e1ca
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2025-12-13 02:20:47 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 113.99 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -2,14 +2,26 @@ import java.util.*;
 
 class Solution {
     
-    static Map<Integer, Integer> map = new HashMap<>();
-    static int[][] visited;
-    static int idx = 1;
+    static final int[] dx = {0, 0, -1, 1};
+    static final int[] dy = {-1, 1, 0, 0};
     
     public int solution(int[][] land) {
         
-        visited = new int[land.length][land[0].length];
-        bfs(land);
+        int[][] visited = new int[land.length][land[0].length];
+        Map<Integer, Integer> map = new HashMap<>();
+	       
+	      int idx = 1;
+	      
+        for (int i = 0; i < land.length; i++) {
+		        for (int j = 0; j < land[0].length; j++) {
+				        if (land[i][j] == 1 && visited[i][j] == 0) {
+						        int cnt = bfs(land, visited, i, j, idx);
+						        map.put(idx, cnt);
+                    idx++;
+				        }
+		        }
+	        
+        }
 
         int max = 0;
         for (int i = 0; i < land[0].length; i++) {
@@ -24,52 +36,39 @@ class Solution {
                 sum += map.get(s);
             }
 
-            if (sum > max) {
-                max = sum;
-            }
+            max = Math.max(max, sum);
         }
 
         return max;
     }
     
-    static void bfs(int[][] land) {
+    static int bfs(int[][] land, int[][] visited,
+                    int x, int y, int idx) {
 
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
+        int cnt = 1;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = idx;
 
-        for (int i = 0; i < land.length; i++) {
-            for (int j = 0; j < land[0].length; j++) {
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int curr_x = curr[0];
+            int curr_y = curr[1];
+            for (int z = 0; z < 4; z++) {
+                int nx = curr_x + dx[z];
+                int ny = curr_y + dy[z];
 
-                if (land[i][j] == 1 && visited[i][j] == 0) {
-                    int cnt = 1;
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[]{i, j});
+                if (nx >= 0 && nx < land.length
+                        && ny >= 0 && ny < land[0].length
+                        && land[nx][ny] == 1
+                        && visited[nx][ny] == 0) {
 
-                    visited[i][j] = idx;
-
-                    while (!queue.isEmpty()) {
-                        int[] curr = queue.poll();
-                        int x = curr[0];
-                        int y = curr[1];
-                        for (int z = 0; z < 4; z++) {
-                            int nx = x + dx[z];
-                            int ny = y + dy[z];
-
-                            if (nx >= 0 && nx < land.length
-                                && ny >= 0 && ny < land[0].length
-                                && land[nx][ny] == 1 && visited[nx][ny] == 0) {
-                                
-                                queue.offer(new int[]{nx, ny});
-                                visited[nx][ny] = idx;
-                                cnt++;
-                            }
-                        }
-                    }
-                    map.put(idx, cnt);
-                    idx++;
+                    queue.offer(new int[]{nx, ny});
+                    visited[nx][ny] = idx;
+                    cnt++;
                 }
-
             }
         }
+        return cnt;
     }
 }
