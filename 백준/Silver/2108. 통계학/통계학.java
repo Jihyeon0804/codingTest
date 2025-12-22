1f3b68e81043cbbdfe35c54dc237a5b03a47afa9
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2025-12-22 14:36:17 +0900

    [Silver II] Title: 통계학, Time: 676 ms, Memory: 65968 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -4,7 +4,6 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         
@@ -17,7 +16,7 @@ public class Main {
             int m = Integer.parseInt(br.readLine());
             sum += m;
             arr[i] = m;
-            map.put(m, map.getOrDefault(m, 1) + 1);
+            map.put(m, map.getOrDefault(m, 0) + 1);
         }
 
         Arrays.sort(arr);
@@ -26,18 +25,18 @@ public class Main {
         List<Integer> list = map.entrySet().stream()
                 .filter(entry -> entry.getValue().equals(max))
                 .map(Map.Entry::getKey)
-                .sorted(Comparator.reverseOrder())
+                .sorted()
                 .collect(Collectors.toList());
 
         int mean = Math.toIntExact(Math.round((double) sum / n));
         int mid = arr[n / 2];
-        int mode = list.size() != 1 ? list.get(list.size() - 2) : list.get(0);
+        int mode = list.size() > 1 ? list.get(1) : list.get(0);
         int range = arr[n - 1] - arr[0];
 
-        sb.append(mean).append("\n");
-        sb.append(mid).append("\n");
-        sb.append(mode).append("\n");
-        sb.append(range);
+        sb.append(mean).append("\n");       // 산술 평균
+        sb.append(mid).append("\n");        // 중앙값
+        sb.append(mode).append("\n");        // 최빈값
+        sb.append(range).append("\n");      // 범위
 
         System.out.println(sb);
     }
