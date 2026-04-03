975d6c7b89844bc64cf1e62e2b09db4d6933a4e1
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2026-04-04 04:22:48 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 592 ms, Memory: 128452 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int[] inDegree = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            graph.get(from).add(to);
+
+            inDegree[to]++;
+        }
+
+        topologicalSort(n, graph, inDegree);
+
+    }
+
+    public static void topologicalSort(int n, List<List<Integer>> graph, int[] inDegree) {
+        int[] resultTime = new int[n + 1];
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+                resultTime[i] = 1;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+
+            for (int next : graph.get(curr)) {
+                resultTime[next] = Math.max(resultTime[curr], resultTime[curr] + inDegree[next]);
+                inDegree[next]--;
+
+                if (inDegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < resultTime.length; i++) {
+            sb.append(resultTime[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
